e29fd822560cbe96a08c9a8d3769bfee69055527
metakunal <kunal99356@gmail.com>
2024-04-29 15:38:49 +0530

    L10 CSS and Conditional Styles

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,11 +1,33 @@
 <script>
-    let flag=true;
-</script>
-
-{#if flag}
-<div>
-    <p>Hey this a modal</p>
-</div>
-{/if}
-
-<style></style>
+    let showModal = true;
+    let isPromo = true;
+  </script>
+  
+  {#if showModal}
+    <div class="backdrop" class:promo={isPromo}>
+      <div class="modal">
+        <p>Sign up for offers!</p>
+      </div>
+    </div>
+  {/if}
+  
+  <style>
+    .backdrop{
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      background: rgba(0,0,0,0.8);
+    }
+    .modal{
+      padding: 10px;
+      border-radius: 10px;
+      max-width: 400px;
+      margin: 10% auto;;
+      text-align: center;
+      background: white;
+    }
+    .promo .modal{
+      background: crimson;
+      color: white;
+    }
+  </style>
